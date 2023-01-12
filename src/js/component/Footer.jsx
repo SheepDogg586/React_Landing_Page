7f6a569954dccaa38646469cdc65b1a07ea4eb62
SheepDogg586 <cole8432@gmail.com>
2023-01-12 15:55:05 +0000

    Landing page initial push

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Footer(){
+    return (
+        <footer className="footer">
+			<p>Copyright Your Website 2022</p>
+		</footer>
+)}
+export default Footer;
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Jumbotron(){
+    return (
+		<div className="text-dark p-5 mt-4 mb-4 bg-secondary bg-opacity-25 rounded-3">
+    		<div className="jumbotron">
+				<h1 className="display-4">A Warm Welcome!</h1>
+				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+				<button className="btn btn-primary">Call to action!</button>
+			</div> 
+		</div>
+)}
+export default Jumbotron;
